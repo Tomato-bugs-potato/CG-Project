e4e04d870b2f6e38d731369ef9118734315f2190
MichaelWorkineh_git <122392569+MichaelWorkineh@users.noreply.github.com>
2024-12-17 10:16:46 +0100

    home page fundamentals

---
+++ Frontend/src/assets/HomePageAssets/HeroCanvas.jsx
@@ -0,0 +1,95 @@
+import React, { useRef } from "react";
+import * as THREE from 'three'
+import { useFrame } from "@react-three/fiber";
+import { ScrollControls, Scroll, useScroll, Center, AccumulativeShadows, RandomizedLight, Text, Html } from "@react-three/drei";
+import Footting from "./Footting"
+
+function HeroCanvas() {
+  return (
+    <>
+    <ScrollControls pages={2} damping={1}>
+      <Scroll>
+        {/* Camera will zoom out as this content is revealed */}
+        <CameraMovement/>
+        <Content/>
+      </Scroll>
+    </ScrollControls>
+    </>
+  );
+}
+
+function CameraMovement() {
+    const scroll = useScroll();
+    //const cameraRef = useRef();
+
+    useFrame((state) => {
+            const scrollY = scroll.offset;
+            state.camera.position.z = 5 + scrollY * 10;
+        }
+    );
+    return null;
+}
+
+function Content3D() {
+  return (
+    <>
+      {/* Section 1 */}
+      <mesh position={[0, 0, 0]}>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color="orange" />
+      </mesh>
+
+      {/* Section 2 */}
+      <mesh position={[0, 0, 5]}>
+        <sphereGeometry args={[0.1, 32, 32]} />
+        <meshStandardMaterial color="blue" />
+      </mesh>
+
+      {/* Section 3 */}
+      <mesh position={[0, 0, 9]}>
+        <coneGeometry args={[1, 2, 32]} />
+        <meshStandardMaterial color="green" />
+      </mesh>
+    </>
+  );
+}
+
+function Sphere(props) {
+    return(
+        <mesh {...props}>
+            <sphereGeometry args={[1, 32, 32]} />
+            <meshStandardMaterial/>
+        </mesh>
+    )
+}
+function Content(){
+  return (
+    <group position={[0, -1, 4]}>
+        <Sphere scale={0.25} position={[-3, 0, 2]}/>
+        <Sphere scale={0.25} position={[-4, 0, -2]}/>
+        <Sphere scale={0.25} position={[3.5, 0, -2]}/>
+        <Text position={[0, 2, -10]} fontSize={5}>
+            :Name_
+            <meshStandardMaterial color="black"/>
+        </Text>
+
+        
+        
+    </group>
+  );
+}
+
+function StaticBackground() {
+  return (
+    <>
+      <ambientLight intensity={0.5} />
+      <pointLight position={[10, 10, 10]} />
+      <mesh position={[0, 0, -10]}>
+        <planeGeometry args={[50, 50]} />
+        <meshStandardMaterial color="lightblue" />
+      </mesh>
+    </>
+  );
+}
+
+export default HeroCanvas;
--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,5 +1,25 @@
 import React from "react";
-
+import { Canvas } from "@react-three/fiber";
+import HeroCanvas from "../assets/HomePageAssets/HeroCanvas";
+import { Environment, OrbitControls } from "@react-three/drei";
+import * as THREE from 'three'
+import Footing from '../assets/HomePageAssets/Footting'
 export default function HomePage() {
-  return <div>HomePage</div>;
+    return (
+        <>
+            <div>
+                <div className="fixed top-0 left-0 w-screen h-screen">
+                    <Footing/>
+                    <Canvas shadows camera={{ position: [0, 0, 0] }}>
+                        <color attach="background" args={["#f0f0f0"]} />
+                        <ambientLight intensity={1} />
+                        <spotLight position={[10, 10, 10]} angle={0.5} penumbra={1} castShadow/>
+                        <pointLight position={[-10, 10, -10]} intensity={2}/>
+                        <HeroCanvas/>
+                        {/* turn this off when you are working offline */}
+                    </Canvas>
+                </div>
+            </div>
+        </>
+    );
 }
